096c44261de4cc993199740c9570b80e334883e1
Lynncept77 <95460649+Lynncept77@users.noreply.github.com>
2022-01-07 11:51:15 +0530

    Update antiflood.py

--- Tanji/modules/antiflood.py
+++ Tanji/modules/antiflood.py
@@ -13,7 +13,7 @@ from Tanji.modules.helper_funcs.chat_status import (
 )
 from Tanji.modules.log_channel import loggable
 from Tanji.modules.sql import antiflood_sql as sql
-from Tanji.error import BadRequest
+from Tanji.errors import BadRequest
 from telegram.ext import (
     CallbackContext,
     CallbackQueryHandler,
