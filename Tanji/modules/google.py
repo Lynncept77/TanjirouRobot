989a501117dec499ad03c32893b19d82059366d8
Lynncept77 <95460649+Lynncept77@users.noreply.github.com>
2022-01-09 13:41:10 +0530

    Update google.py

--- Tanji/modules/google.py
+++ Tanji/modules/google.py
@@ -287,7 +287,7 @@ __help__ = """
 ❂ /github <username>*:* Get information about a GitHub user.
 ❂ /country <country name>*:* Gathering info about given country
 ❂ /imdb <Movie name>*:* Get full info about a movie with imdb.com
-❂ Siesta <query>*:* Siesta answers the query
+❂ Tanjirou <query>*:* Tanjirou answers the query
 
-  💡Ex: `Siesta where is Japan?`
+  💡Ex: `Tanjirou where is Japan?`
 """
